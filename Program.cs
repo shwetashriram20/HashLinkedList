7cf7aa9c6a30f2764f5b1dfcf37b6460e45b809a
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-13 18:42:13 +0530

    Operation

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LinkedList
+{
+    class Program
+{
+    public static void Main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+
+            list.insertNode(56);
+            list.insertNode(30);
+            list.insertNode(40);
+            list.insertNode(70);
+            list.printNodes();
+
+            list.orderNodes();
+
+        }
+    }
+
+}
+          
+
