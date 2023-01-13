2c1c84d58fc86a40e150c6794b83fff21c3ce3d2
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-13 19:13:51 +0530

    Add files via upload

---
+++ LinkedMap.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashLinkedList
+{
+    public class LinkedMap<K, V> where K : IComparable
+    {
+        private readonly int NumBuckets;
+        readonly List<LinkedList<K, V>> BucketList;
+        public StringBuilder Result;
+
+        public LinkedMap(int NumBuckets)
+        {
+            this.NumBuckets = NumBuckets;
+            BucketList = new List<LinkedList<K, V>>(NumBuckets);
+            Result = new StringBuilder();
+
+            for (int i = 0; i < NumBuckets; i++)
+                BucketList.Add(null);
+        }
+        public V Get(K Key)
+        {
+            int Index = GetIndex(Key);
+            LinkedList<K, V> linkedlist = BucketList[Index];
+            if (linkedlist == null)
+                return default;
+            HashNode<K, V> hashnode = linkedlist.Search(Key);
+            return (hashnode == null) ? default : hashnode.Value;
+        }
+        public int GetIndex(K Key)
+        {
+            int HashCode = Math.Abs(Key.GetHashCode());
+            int Index = HashCode % NumBuckets;
+            return Index;
+        }
+        public void Add(K Key, V Value)
+        {
+            int Index = GetIndex(Key);
+            LinkedList<K, V> linkedlist = BucketList[Index];
+
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<K, V>();
+                BucketList[Index] = linkedlist;
+            }
+            HashNode<K, V> hashnode = linkedlist.Search(Key);
+            if (hashnode == null)
+            {
+                hashnode = new HashNode<K, V>(Key, Value);
+                linkedlist.Append(hashnode);
+            }
+            else hashnode.Value = Value;
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HashLinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hashtable Program");
+            LinkedMap<string, int> linkedmap = new LinkedMap<string, int>(5);
+
+            string Sentence = "paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] Words = Sentence.Split(" ");
+            Console.Write("Enter the word you want to omit: ");
+            string BannedWord = Console.ReadLine();
+            foreach (string word in Words)
+            {
+                if (word != BannedWord)
+                {
+                    int Value = linkedmap.Get(word);
+                    if (Value == default)
+                        Value = 1;
+                    else Value++;
+                    linkedmap.Add(word, Value);
+                }
+            }
+            int Frequency = linkedmap.Get(BannedWord);
+            Console.WriteLine("\n\"{0}\" comes {1} times in the given paragraph now",
+                BannedWord, Frequency);
+            Console.WriteLine("\nResulting paragraph:\n" + linkedmap.Result);
+        }
+    }
+}
+
